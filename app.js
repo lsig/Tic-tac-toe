4f5cf4afb979b3508341fccf66d5e5399483fb33
Lsig <logis21@ru.is>
2023-04-24 16:03:12 +0200

    Tic tac toe functinolity added, displays winner and draws

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-const gameBoard = (() => {
+const player = (mark) => {
+  const getMark = () => mark;
+  return { getMark };
+};
+
+const grid = (() => {
   const gameboard = ["", "", "", "", "", "", "", "", ""];
-  let win = false;
+  let count = 0;
+
   let draw = false;
+  const player_one = player("X");
+  const player_two = player("O");
+  const players = [player_one, player_two];
 
   const getGameboard = () => gameboard;
+  const getPlayer = () => {
+    const currentPlayer = players[count];
+    return currentPlayer;
+  };
   const boardNotFull = () => gameboard.some((val) => val === "");
   const setTile = (mark, index) => {
     if (!gameboard[index]) {
       gameboard[index] = mark;
+      count = (count + 1) % 2;
     }
-    if (!boardNotFull) {
+    if (!boardNotFull()) {
       draw = true;
     }
   };
   const checkWinner = (mark) => {
+    let win = false;
     const winningCombos = [
       [0, 1, 2],
       [3, 4, 5],
@@ -25,28 +40,73 @@ const gameBoard = (() => {
       [2, 4, 6],
     ];
     winningCombos.forEach((combo) => {
-      const playerCombo = combo.map((index) => gameBoard[index]);
+      const playerCombo = combo.map((index) => gameboard[index]);
       const winner = playerCombo.every((val) => val === mark);
+      console.log(playerCombo);
+      console.log(winner);
       if (winner) {
         win = true;
         return;
       }
     });
+
     return win;
   };
 
   const checkDraw = () => draw;
 
-  return { getGameboard, setTile, checkWinner, checkDraw };
+  return { players, getPlayer, getGameboard, setTile, checkWinner, checkDraw };
 })();
 
-const player = (mark) => {
-  let winner = false;
-  const setMark = () => {
-    gameBoard.setTile(mark);
-    winner = gameBoard.checkWinner(mark);
-  };
-  return { setMark };
+const displayController = (() => {
+  const tiles = document.querySelectorAll(".tile");
+  tiles.forEach((tile) => {
+    tile.addEventListener("click", () => {
+      const currentPlayer = grid.getPlayer();
+      const playerMark = currentPlayer.getMark();
+      const tileNumber = getTileNum(tile);
+      const board = grid.getGameboard();
+
+      if (board[tileNumber] !== "") {
+        return;
+      }
+
+      grid.setTile(playerMark, tileNumber);
+      gridElement = createMarkElement(playerMark);
+      tile.appendChild(gridElement);
+
+      const win = grid.checkWinner(playerMark);
+      const draw = grid.checkDraw();
+
+      if (win) {
+        alert(`Congrats, player ${playerMark} won`);
+        reloadPage();
+        return;
+      }
+      if (draw) {
+        alert("The game is a draw");
+        reloadPage();
+        return;
+      }
+    });
+  });
+})();
+
+const createMarkElement = (mark) => {
+  const element = document.createElement("h1");
+  const elementText = document.createTextNode(mark);
+  element.appendChild(elementText);
+  return element;
 };
 
-const displayController = (() => {})();
+const reloadPage = () => {
+  setTimeout(() => {
+    location.reload();
+  }, 2000);
+};
+
+const getTileNum = (tile) => {
+  const tileId = tile.id;
+  const tileNumber = parseInt(tileId.slice(-1));
+  return tileNumber;
+};
